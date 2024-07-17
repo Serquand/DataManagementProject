ca44ce74c2fd48fe16d59a89fc325e0154b39227
Serquand <esteban.vincent@efrei.net>
2024-07-17 16:11:32 +0200

    Refactor  code

--- queries.py
+++ queries.py
@@ -5,6 +5,49 @@ first_pipeline = [
     {
         "$group": {
             "_id": "$to_ip",
+            "count": {"$sum": 1 }
+        }
+    },
+    {"$sort": {"count": -1}},
+]
+
+second_pipeline = [
+    {"$match": {"predictions": "normal"}},
+    {
+        "$group": {
+            "_id": "$to_ip",
+            "count": {"$sum": 1}
+        }
+    },
+    {"$sort": {"count": -1}},
+]
+
+third_pipeline = [
+    {"$match": {"predictions": "anomaly"}},
+    {
+        "$group": {
+            "_id": "$from_ip",
+            "count": {"$sum": 1}
+        }
+    },
+    {"$sort": {"count": -1}},
+]
+
+fourth_pipeline = [
+    {
+        "$group": {
+            "_id": "$to_ip",
+            "count": {"$sum": 1}
+        }
+    },
+    {"$sort": {"count": -1}}
+]
+
+fifth_pipeline = [
+    {"$match": {"predictions": "normal"}},
+    {
+        "$group": {
+            "_id": "$from_ip",
             "count": {"$sum": 1}
         }
     },
@@ -12,210 +55,56 @@ first_pipeline = [
     {"$limit": 10}
 ]
 
-df6 = query6('DataManagement-Project', 'AfterTreatment')
-print(df6)
-
-
-def query7(db_name, collection_name, connection_string='mongodb://localhost:27017/'):
-    client = MongoClient(connection_string)
-    db = client[db_name]
-    collection = db[collection_name]
-
-    pipeline = [
-        {"$match": {"predictions": "normal"}},
-        {
-            "$group": {
-                "_id": "$to_ip",
-                "count": {"$sum": 1}
-            }
-        },
-        {"$sort": {"count": -1}},
-        {"$limit": 10}
-    ]
-
-    result = list(collection.aggregate(pipeline))
-    return result
-
-df7 = query7('DataManagement-Project', 'AfterTreatment')
-print(df7)
-
-def query4(db_name, collection_name, connection_string='mongodb://localhost:27017/'):
-    client = MongoClient(connection_string)
-    db = client[db_name]
-    collection = db[collection_name]
-
-    pipeline = [
-        {"$match": {"predictions": "anomaly"}},
-        {
-            "$group": {
-                "_id": "$from_ip",
-                "count": {"$sum": 1}
-            }
-        },
-        {"$sort": {"count": -1}},
-        {"$limit": 10}
-    ]
-
-    result = list(collection.aggregate(pipeline))
-    print(result)
-
-df4 = query4('DataManagement-Project', 'AfterTreatment')
-print(df4)
-
-def query3(db_name, collection_name, connection_string='mongodb://localhost:27017/'):
-    client = MongoClient(connection_string)
-    db = client[db_name]
-    collection = db[collection_name]
-
-    pipeline = [
-        {
-            "$group": {
-                "_id": "$to_ip",
-                "count": {"$sum": 1}
-            }
-        },
-        {"$sort": {"count": -1}}
-    ]
-
-    result = list(collection.aggregate(pipeline))
-    print(result)
-
-df3 = query3('DataManagement-Project', 'AfterTreatment')
-print(df3)
-
-def query5(db_name, collection_name, connection_string='mongodb://localhost:27017/'):
-    client = MongoClient(connection_string)
-    db = client[db_name]
-    collection = db[collection_name]
-
-    pipeline = [
-        {"$match": {"predictions": "normal"}},
-        {
-            "$group": {
-                "_id": "$from_ip",
-                "count": {"$sum": 1}
-            }
-        },
-        {"$sort": {"count": -1}},
-        {"$limit": 10}
-    ]
-
-    result = list(collection.aggregate(pipeline))
-    print(result)
-
-df5 = query5('DataManagement-Project', 'AfterTreatment')
-print(df5)
-
-import pandas as pd
-from pymongo import MongoClient
-
-def query1(db_name, collection_name, connection_string='mongodb://localhost:27017/'):
-    client = MongoClient(connection_string)
-    db = client[db_name]
-    collection = db[collection_name]
-
-    pipeline = [
-        {
-            "$group": {
-                "_id": {
-                    "predictions": "$predictions",
-                    "protocol_type": "$protocol_type",
-                    "service": "$service",
-                    "flag": "$flag"
-                },
-                "count": {"$sum": 1}
-            }
-        },
-        {"$sort": {"count": -1}},
-        {
-            "$group": {
-                "_id": "$_id.predictions",
-                "most_common": {
-                    "$first": {
-                        "protocol_type": "$_id.protocol_type",
-                        "service": "$_id.service",
-                        "flag": "$_id.flag",
-                        "count": "$count"
-                    }
-                }
-            }
-        },
-        {
-            "$replaceRoot": {
-                "newRoot": {
-                    "predictions": "$_id",
-                    "protocol_type": "$most_common.protocol_type",
-                    "service": "$most_common.service",
-                    "flag": "$most_common.flag",
-                    "count": "$most_common.count"
-                }
-            }
+sixth_pipeline = [
+    {
+        "$group": {
+            "_id": "$from_ip",
+            "count": {"$sum": 1}
         }
-    ]
+    },
+    {"$sort": {"count": -1}}
+]
 
-    result = list(collection.aggregate(pipeline))
-    print(result)
-
-df1 = query1('DataManagement-Project', 'AfterTreatment')
-print(df1)
-
-def query2(db_name, collection_name, connection_string='mongodb://localhost:27017/'):
-    client = MongoClient(connection_string)
-    db = client[db_name]
-    collection = db[collection_name]
-
-    pipeline = [
-        {
-            "$group": {
-                "_id": "$from_ip",
-                "count": {"$sum": 1}
-            }
-        },
-        {"$sort": {"count": -1}}
-    ]
-
-    result = list(collection.aggregate(pipeline))
-    print(result)
-
-def get_ip_communication_count(db_name, collection_name, connection_string='mongodb://localhost:27017/'):
-    # Connexion à MongoDB
-    client = MongoClient(connection_string)
-    db = client[db_name]
-    collection = db[collection_name]
-
-    # Exécution de l'agrégation
-    pipeline = [
-        {
-            "$group": {
-                "_id": {"from_ip": "$from_ip", "to_ip": "$to_ip"},
-                "count": {"$sum": 1}
-            }
-        },
-        {
-            "$project": {
-                "_id": 0,
-                "from_ip": "$_id.from_ip",
-                "to_ip": "$_id.to_ip",
-                "count": "$count"
-            }
-        },
-        {
-            "$sort": {"count": -1}
+seventh_pipeline = [
+    {
+        "$group": {
+            "_id": {"from_ip": "$from_ip", "to_ip": "$to_ip"},
+            "count": {"$sum": 1}
         }
-    ]
-
-    result = list(collection.aggregate(pipeline))
-    print(result)
-
-df = get_ip_communication_count(db_name, collection_name)
-print(df)
+    },
+    {
+        "$project": {
+            "_id": 0,
+            "from_ip": "$_id.from_ip",
+            "to_ip": "$_id.to_ip",
+            "count": "$count"
+        }
+    },
+    {
+        "$sort": {"count": -1}
+    }
+]
 
 def execute_request(pipeline, collection):
     return list(collection.aggregate(pipeline))
-
 
 if __name__ == '__main__':
     client = MongoClient('mongodb://localhost:27017/')
     db = client['DataManagement-Project']
     collection = db['AfterTreatment']
 
+    first_result = execute_request(first_pipeline, collection)
+    second_result = execute_request(second_pipeline, collection)
+    third_result = execute_request(third_pipeline, collection)
+    fourth_result = execute_request(fourth_pipeline, collection)
+    fifth_result = execute_request(fifth_pipeline, collection)
+    sixth_result = execute_request(sixth_pipeline, collection)
+    seventh_result = execute_request(seventh_pipeline, collection)
+
+    print("First :", str(first_result))
+    print("\nSecond :", str(second_result))
+    print("\nThird :", str(third_result))
+    print("\nFourth :", str(fourth_result))
+    print("\nFifth :", str(fifth_result))
+    print("\nSixth :", str(sixth_result))
+    print("\nSeventh :", str(seventh_result))
