e1c3c55c09924ab795fea36367fd316b85cfa3a6
Serquand <esteban.vincent@efrei.net>
2024-07-17 15:45:04 +0200

    Generate From & To IPs

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
 import pandas as pd
 import joblib
 from pymongo import MongoClient
+import random
 import warnings
 from sklearn.exceptions import InconsistentVersionWarning
 
 warnings.filterwarnings("ignore", category=InconsistentVersionWarning)
+
+def generate_all_ips(number_of_ip_to_generate):
+    from_ips = []
+    to_ips = []
+    for _ in range(number_of_ip_to_generate):
+        from_ip = generate_random_ip(None)
+        from_ips.append(from_ip)
+        to_ips.append(generate_random_ip(from_ip))
+    return {"from_ips": from_ips, "to_ips": to_ips}
+
+def generate_random_ip(exclude_ip: str | None) -> str:
+    list_ips = ["127.0.0.1", "127.0.0.2", "127.0.0.3", "127.0.0.4"]
+    if exclude_ip is None:
+        return list_ips[random.randint(0, 3)]
+    else:
+        while True:
+            chosen_ip = list_ips[random.randint(0, 3)]
+            if chosen_ip != exclude_ip:
+                return chosen_ip
 
 def treat_data(data):
     model = joblib.load('best_model.joblib')
@@ -14,6 +34,9 @@ def treat_data(data):
     predictions = model.predict(X_new)
     predicted_labels = ['anomaly' if x == -1 else 'normal' for x in predictions]
     df['predictions'] = predicted_labels
+    ips = generate_all_ips(len(predictions))
+    df["from_ip"] = ips["from_ips"]
+    df["to_ip"] = ips["to_ips"]
     return df
 
 def load_data_from_db(db):
